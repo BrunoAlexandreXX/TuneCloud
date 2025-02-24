d010154cdceaaefdfd54af56608604d873a07a06
Bruno Alexandre <101072146+BrunoAlexandreXX@users.noreply.github.com>
2025-02-23 21:51:09 -0300

    Adicionar login e registro de usuários com Firebase

---
+++ firebase.js
@@ -0,0 +1,43 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBqIAcrQK2J50sP-zKhXvmKp3BwDNzhUGU",
+  authDomain: "tunecloud-b5ab2.firebaseapp.com",
+  projectId: "tunecloud-b5ab2",
+  storageBucket: "tunecloud-b5ab2.firebasestorage.app",
+  messagingSenderId: "648006663847",
+  appId: "1:648006663847:web:f57f8ad0b6f6a6235d2582",
+  measurementId: "G-WWX9CL2EWT"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBqIAcrQK2J50sP-zKhXvmKp3BwDNzhUGU",
+  authDomain: "tunecloud-b5ab2.firebaseapp.com",
+  projectId: "tunecloud-b5ab2",
+  storageBucket: "tunecloud-b5ab2.firebasestorage.app",
+  messagingSenderId: "648006663847",
+  appId: "1:648006663847:web:f57f8ad0b6f6a6235d2582",
+  measurementId: "G-WWX9CL2EWT"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
